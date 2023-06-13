ca77f9a151799ec083d87d64f03c9447af986445
Umetani Kanata <c0a22024c6@edu.teu.ac.jp>
2023-06-13 17:31:35 +0900

    演習

--- pygame_image.py
+++ pygame_image.py
@@ -7,6 +7,7 @@ def main():
     screen = pg.display.set_mode((800, 600))
     clock  = pg.time.Clock()
     bg_img = pg.image.load("ex01/fig/pg_bg.jpg")
+    bg_img2= pg.transform.flip(bg_img,True,False)
     kk_img = pg.image.load("ex01/fig/3.png")
     kk_img = pg.transform.flip(kk_img,True,False)
     kk_rt = pg.transform.rotozoom(kk_img,10,1.0)
@@ -19,12 +20,13 @@ def main():
             if event.type == pg.QUIT: return
 
         screen.blit(bg_img, [-x, 0])
-        screen.blit(bg_img, [1600-x,0])
+        screen.blit(bg_img2, [1600-x,0])
+        screen.blit(bg_img,[3200-x,0]) 
         screen.blit(chiken[tmr%2],[300,200])
         pg.display.update()
         tmr += 1      
-        x += 10  
-        clock.tick(10)
+        x = tmr%3200
+        clock.tick(20)
 
 
 if __name__ == "__main__":
