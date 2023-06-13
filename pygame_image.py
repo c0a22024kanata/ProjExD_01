3eb9fa739f79f6909222e07af289bec7da2df84a
Umetani Kanata <c0a22024c6@edu.teu.ac.jp>
2023-06-13 16:57:13 +0900

    練習６

--- pygame_image.py
+++ pygame_image.py
@@ -11,16 +11,19 @@ def main():
     kk_img = pg.transform.flip(kk_img,True,False)
     kk_rt = pg.transform.rotozoom(kk_img,10,1.0)
     tmr = 0
+    x = 0
     chiken = [kk_img,kk_rt]
 
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
 
-        screen.blit(bg_img, [0, 0])
+        screen.blit(bg_img, [-x, 0])
+        screen.blit(bg_img, [1600-x,0])
         screen.blit(chiken[tmr%2],[300,200])
         pg.display.update()
-        tmr += 1        
+        tmr += 1      
+        x += 10  
         clock.tick(10)
 
 
